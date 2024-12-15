b296a1eff817512c2418b58c027aeb9bcaf76055
Erik Martus <erik@emartus.dev>
2024-12-15 14:54:23 -0500

    fix group slug in recipe tile

--- src/components/RecipeTile.tsx
+++ src/components/RecipeTile.tsx
@@ -6,7 +6,7 @@ export default function RecipeTile({ recipe }: { recipe: RecipeContent }) {
   return (
     <Tile
       name={recipe.title}
-      href={`/groups/${recipe.group}/recipes/${recipe.slug}`}
+      href={`/groups/${recipe.group.slug}/recipes/${recipe.slug}`}
     >
       {recipe.tags && <TagRow tags={recipe.tags} limit={2} />}
     </Tile>
