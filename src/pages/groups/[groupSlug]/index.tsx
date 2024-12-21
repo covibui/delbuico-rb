08a7c103dbd628b5a8552e98278ac083dc9b745b
Erik Martus <erik@emartus.dev>
2024-12-21 16:01:47 -0500

    group empty state

--- src/pages/groups/[groupSlug]/index.tsx
+++ src/pages/groups/[groupSlug]/index.tsx
@@ -2,7 +2,7 @@ import Layout from "@components/Layout";
 import { getGroup, listGroups } from "@lib/groups";
 import { listRecipeCacheMeta } from "@lib/recipes";
 import { GetStaticPaths, GetStaticProps } from "next";
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import RecipeTile from "@components/RecipeTile";
 import { GroupMeta, RecipeMeta } from "src/types";
 import { getTags } from "@lib/tags";
@@ -17,11 +17,17 @@ export default function Group({ group, recipes }: Props) {
   return (
     <Layout title={group.name} itemCount={group.count} backLink="/">
       <BasicMeta title={group.name} url={`/groups/${group.slug}`} />
-      <Box sx={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: 2.5 }}>
-        {recipes.map((recipe, idx) => (
-          <RecipeTile key={idx} recipe={recipe} />
-        ))}
-      </Box>
+      {recipes.length > 0 ? (
+        <Box sx={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: 2.5 }}>
+          {recipes.map((recipe, idx) => (
+            <RecipeTile key={idx} recipe={recipe} />
+          ))}
+        </Box>
+      ) : (
+        <Stack alignItems="center">
+          <Typography>No recipes found</Typography>
+        </Stack>
+      )}
     </Layout>
   );
 }
